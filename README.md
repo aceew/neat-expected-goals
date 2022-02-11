46f85aa1d9ebfed6e8199967296138171e3f299b
aceew <aaronwilliams9797@gmail.com>
2022-02-11 16:14:54 +0000

    feature: Switched neat library, built network based on some base figures and buitl prediction code

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # NEAT Expected Goals Model
 
 The purpose of this is to build a model that predicts match results. It will use historic data and a comparison of data points to predict which team will win a match based on current form of expected goals.
+
+## Architecture of the model
+
+1. Model per team. Always play team B against team A's network and then do the same vice versa to compare what each network believes the final score will be.
+2. Use makhov chain data to pass in historical data into probabilities over time
--- index.js
+++ index.js
@@ -1,32 +1,79 @@
-const NEAT = require('neat_net-js');
-const populationSize = 100;
+const Neat = require('justneat');
+const Genome = require('justneat/models/neatGenome')
+const Client = require('justneat/models/client')
 
-const dummyMatches = [
-  {homeTeam: "West Brom", },
-  {},
-  {},
-  {},
-  {},
-]
+const fs = require('fs');
+const data = require('./data/2019.json');
+const outputFolder = "./output"
+
+const getMatchInputs = (match) => {
+  const homePossession = match.team_a_possession / 100
+  const homeAttacks = match.team_a_attacks
+  const awayAttacks = match.team_b_attacks
+  const homePrematchXG = match.team_a_xg_prematch
+  const awayPrematchXG = match.team_b_xg_prematch
+  const homePrematchPPG = match.pre_match_home_ppg
+  const awayPrematchPPG = match.pre_match_away_ppg
+
+  const expectedHomeXG = match.team_a_xg
+  const expectedAwayXG = match.team_b_xg
+
+  return {
+    inputs: {
+      homePossession,
+      homeAttacks,
+      awayAttacks,
+      homePrematchXG,
+      awayPrematchXG,
+      homePrematchPPG,
+      awayPrematchPPG,
+    },
+    expectedOutputs: {
+      expectedHomeXG,
+      expectedAwayXG
+    }
+  }
+}
 
 const run = () => {
-  const config = {
-    model: [
-      { nodeCount: 5, type: "input" },
-      { nodeCount: 2, type: "output", activationfunc: NEAT.activation.SOFTMAX }
-    ],
-    mutationRate: 0.1,
-    crossoverMethod: NEAT.crossover.RANDOM,
-    mutationMethod: NEAT.crossover.RANDOM,
-    populationSize: populationSize
+  let neat;
+  const { inputs: inputObject, expectedOutputs} = getMatchInputs(data.data[0])
+
+  const expected = Object.values(expectedOutputs)
+  const inputs = Object.values(inputObject)
+  const expectedOutputLength = expected.length
+  const accuracy = 0.01
+
+  const testCaseName = `${Object.keys(inputObject).join('_')}_${expectedOutputLength}_${accuracy}`
+  const networkFilename = `${outputFolder}/${testCaseName}_network.json`
+  const resultFilename = `${outputFolder}/${testCaseName}_result.json`
+
+  const isExisting = fs.existsSync(networkFilename)
+
+  if (isExisting) {
+    const existingData = fs.readFileSync(networkFilename, 'utf-8')
+    neat = Neat.FromJson(existingData)
+  } else {
+    neat = new Neat(inputs.length, expectedOutputLength)
   }
 
-  const neat = new NEAT(config);
-  // for (let i = 0; i < creatureCount; i++) {
-  //   neat.setInputs(ARRAY(INPUT_LAYER_NODE_COUNT), i);  // Set inputs for the creature indexed i.
-  // }
-
-  neat.predict()
+  const best = neat.trainData([[inputs, expected]], accuracy, true)
+  const networkData = neat.toJson()
+  fs.writeFileSync(networkFilename, networkData)
+  fs.writeFileSync(resultFilename, best.client.genome.toJson())
 };
 
-run();
+const predictMatch = () => {
+  // TODO parametise match data
+  const { inputs: inputObject, expectedOutputs } = getMatchInputs(data.data[0])
+
+  // TODO generate on the go
+  const genomeData = require('homePossession_homeAttacks_awayAttacks_homePrematchXG_awayPrematchXG_homePrematchPPG_awayPrematchPPG_2_0.01_result.json')
+  const client = new Client(Genome.FromJson(genomeData))
+  const outputs = client.predict(Object.values(inputObject))
+  console.table({ outputs, expectedOutputs })
+};
+
+// run();
+
+predictMatch();
