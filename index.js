a8a0d8172612322379bb956c8dbeb4aa18ee461a
aceew <aaronwilliams9797@gmail.com>
2022-02-11 16:41:55 +0000

    (wip): Made more dynamic filenames

--- index.js
+++ index.js
@@ -6,28 +6,39 @@ const fs = require('fs');
 const data = require('./data/2019.json');
 const outputFolder = "./output"
 
+const inputsData = {
+  homePossession: 'team_a_possession',
+  homeAttacks: 'team_a_attacks',
+  awayAttacks: 'team_b_attacks',
+  homePrematchXG: 'team_a_xg_prematch',
+  awayPrematchXG: 'team_b_xg_prematch',
+  homePrematchPPG: 'pre_match_home_ppg',
+  awayPrematchPPG: 'pre_match_away_ppg',
+}
+
+const accuracy = 0.160
+const expectedOutputLength = 2
+const testCaseName = `${Object.keys(inputsData).join('_')}_${expectedOutputLength}_${accuracy}`
+const networkFilename = `${outputFolder}/${testCaseName}_network.json`
+const resultFilename = `${outputFolder}/${testCaseName}_result.json`
+
+const gameCount = 5
+
 const getMatchInputs = (match) => {
-  const homePossession = match.team_a_possession / 100
-  const homeAttacks = match.team_a_attacks
-  const awayAttacks = match.team_b_attacks
-  const homePrematchXG = match.team_a_xg_prematch
-  const awayPrematchXG = match.team_b_xg_prematch
-  const homePrematchPPG = match.pre_match_home_ppg
-  const awayPrematchPPG = match.pre_match_away_ppg
+  const inputs = {}
+  Object.keys(inputsData).forEach((key) => {
+    if (key === 'homePossession') {
+      inputs[key] = match.team_a_possession / 100
+    } else {
+      inputs[key] = match[inputsData[key]]
+    }
+  });
 
   const expectedHomeXG = match.team_a_xg
   const expectedAwayXG = match.team_b_xg
 
   return {
-    inputs: {
-      homePossession,
-      homeAttacks,
-      awayAttacks,
-      homePrematchXG,
-      awayPrematchXG,
-      homePrematchPPG,
-      awayPrematchPPG,
-    },
+    inputs,
     expectedOutputs: {
       expectedHomeXG,
       expectedAwayXG
@@ -37,16 +48,11 @@ const getMatchInputs = (match) => {
 
 const run = () => {
   let neat;
-  const { inputs: inputObject, expectedOutputs} = getMatchInputs(data.data[0])
 
-  const expected = Object.values(expectedOutputs)
-  const inputs = Object.values(inputObject)
-  const expectedOutputLength = expected.length
-  const accuracy = 0.01
-
-  const testCaseName = `${Object.keys(inputObject).join('_')}_${expectedOutputLength}_${accuracy}`
-  const networkFilename = `${outputFolder}/${testCaseName}_network.json`
-  const resultFilename = `${outputFolder}/${testCaseName}_result.json`
+  const testMatchData = data.data.slice(0, gameCount).map((match) => {
+    const { inputs, expectedOutputs} = getMatchInputs(match)
+    return [Object.values(inputs), Object.values(expectedOutputs)]
+  })
 
   const isExisting = fs.existsSync(networkFilename)
 
@@ -54,10 +60,10 @@ const run = () => {
     const existingData = fs.readFileSync(networkFilename, 'utf-8')
     neat = Neat.FromJson(existingData)
   } else {
-    neat = new Neat(inputs.length, expectedOutputLength)
+    neat = new Neat(testMatchData[0][0].length, expectedOutputLength)
   }
 
-  const best = neat.trainData([[inputs, expected]], accuracy, true)
+  const best = neat.trainData(testMatchData, accuracy, true)
   const networkData = neat.toJson()
   fs.writeFileSync(networkFilename, networkData)
   fs.writeFileSync(resultFilename, best.client.genome.toJson())
@@ -68,8 +74,8 @@ const predictMatch = () => {
   const { inputs: inputObject, expectedOutputs } = getMatchInputs(data.data[0])
 
   // TODO generate on the go
-  const genomeData = require('homePossession_homeAttacks_awayAttacks_homePrematchXG_awayPrematchXG_homePrematchPPG_awayPrematchPPG_2_0.01_result.json')
-  const client = new Client(Genome.FromJson(genomeData))
+  const genomeData = require(resultFilename)
+  const client = new Client(Genome.FromJson(JSON.stringify(genomeData)))
   const outputs = client.predict(Object.values(inputObject))
   console.table({ outputs, expectedOutputs })
 };
